ca4497f2d33c6e473bbeacf10f9199dc4df9fa12
“LokeshaRasanjalee” <rlokesha7@gmail.com>
2021-03-23 12:35:59 +0530

    Ádding mahout code

--- hello.py
+++ hello.py
@@ -11,11 +11,12 @@ p = os.popen("hadoop fs -cat recommendations/part*")
 # Load the recommendations into Redis
 for i in p:
 
-  # Split recommendations into key of user id 
+  # Split recommendations into key of user id
   # and value of recommendations
   # E.g., 35^I[2067:5.0,17:5.0,1041:5.0,2068:5.0,2087:5.0,
   #       1036:5.0,900:5.0,1:5.0,081:5.0,3135:5.0]$
-  k,v = i.split('t')
+  print(i)
+  k,v = i.split('\t')
 
   # Put key, value into Redis
   r.set(k,v)
@@ -33,7 +34,7 @@ def recs(request, id):
 @route('/')
 
 def home(request):
-  return 'Please add a user id to the URL, e.g. http://localhost:8080/1234n'
+  return 'Please add a user id to the URL, e.g. http://localhost:8082/1234n'
 
-# Start up a listener on port 8085
-run("localhost", 8085)
+# Start up a listener on port 8082
+run("localhost", 8082)
